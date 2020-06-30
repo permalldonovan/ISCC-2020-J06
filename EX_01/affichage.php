55f7d1b34b75435a0925889a794845dc78af96a2
permalldonovan <permall.donovan@iseg.fr>
2020-06-30 19:53:46 +0200

    Day06

---
+++ EX_01/affichage.php
@@ -0,0 +1,16 @@
+<?php
+$nom_produit='T-shirt femme';
+$couleur='Rouge';
+$prix=15.50;
+$disponible='true';
+$quantite=10;
+
+//fonction 1//
+
+function affichage($nom_produit) {
+    echo "$variable T-shirt femme";
+}
+
+//action 1//
+
+echo "<h1> Fiche produit: {$nom_produit}</h1>";
---
+++ EX_01/test-fonctions.php
@@ -0,0 +1,7 @@
+<?php
+$nom_produit='T-shirt femme';
+$couleur='Rouge';
+$prix=15.50;
+$disponible='true';
+$quantite=10;
+?>
